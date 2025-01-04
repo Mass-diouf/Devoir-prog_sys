4b052720a24eb4d2211d655989c4ed405e1f1539
Massamba DIOUF <diouf.massamba1@ugb.edu.sn>
2025-01-04 01:20:03 +0000

    Update devoir4.c

--- devoir4.c
+++ devoir4.c
@@ -1,71 +1,50 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdlib.h>
 
-#define N 10  
+sem_t sem1;
 
-int value = 0; 
-sem_t sem1, sem2; 
-int results[2 * N + 1]; 
-int index_result = 0;
-
-void* thread1_function(void* arg) 
-{
-    for (int i = 0; i <= N; i++) 
-    {
-        sem_wait(&sem1); 
-        value++;
-        results[index_result++] = value;
-        printf("Thread 1: %d\n", value);
-        sem_post(&sem2); 
+void *afficher1(void *arg) {
+    int n = *(int *)arg;
+    
+    for (int i = 1; i <= n; i++) {
+        
+        printf("%d \n", i);
+      
     }
+    sem_post(&sem1);
     return NULL;
 }
 
-void* thread2_function(void* arg) 
-{
-    for (int i = 0; i <= N; i++) 
-    {
-        sem_wait(&sem2); 
-        value--;
-        results[index_result++] = value;
-        printf("Thread 2: %d\n", value);
-        sem_post(&sem1); 
+void *afficher2(void *arg) {
+    int n = *(int *)arg;
+    sem_wait(&sem1);
+    for (int i = -1; i >= -n; i--) {
+       
+        printf("%d \n", i);
+        
     }
     return NULL;
 }
 
 int main() {
-    pthread_t thread1, thread2;
+    pthread_t p1, p2;
+    int n = 4;
 
-    
-    sem_init(&sem1, 0, 1); 
-    sem_init(&sem2, 0, 0); 
+    // Initialisation des sémaphores
+    sem_init(&sem1, 1, 0); 
+    pthread_create(&p1, NULL, afficher1, &n);
+    pthread_create(&p2, NULL, afficher2, &n);
 
-    
-    pthread_create(&thread1, NULL, thread1_function, NULL);
-    pthread_create(&thread2, NULL, thread2_function, NULL);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
 
-   
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    printf("Fin du programme\n");
 
    
     sem_destroy(&sem1);
-    sem_destroy(&sem2);
-
-   
-    FILE* file = fopen("results.txt", "w");
-    if (file) {
-        for (int i = 0; i < 2 * N + 1; i++) {
-            fprintf(file, "%d\n", results[i]);
-        }
-        fclose(file);
-        printf("Résultats sauvegardés dans 'results.txt'\n");
-    } else {
-        printf("Erreur d'ouverture du fichier\n");
-    }
+  
 
     return 0;
 }
